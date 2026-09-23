9c96c2b89a39b4dba85babc939f4ed1360981a3c
bharat thapa <bharatthapa45@gmail.com>
2026-09-23 22:44:32 +0545

    chore: set up PHP tooling (Composer, WPCS, PHP compat checks)

--- youtube-video-loader.php
+++ youtube-video-loader.php
@@ -2,24 +2,24 @@
 /**
  * Plugin Name:         YouTube Video Loader
  * Plugin URI:          #
- * Description:         Insert YouTube Videos Efficiently in your site. 
+ * Description:         Insert YouTube Videos Efficiently in your site.
  * Version:             1.0.0
- * Requires at least:   6.8
- * Requires PHP:        8.2
+ * Requires at least:   6.9
+ * Requires PHP:        8.0
  * Author:              Bharat Thapa
  * Author URI:          https://bharatt.com.np
  * License:             GPL V3
  * License URI:         https://www.gnu.org/licenses/gpl-3.0.html
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License version 3, as published by the Free Software Foundation. You may NOT assume
  * that you can use any other version of the GPL.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
+ *
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
